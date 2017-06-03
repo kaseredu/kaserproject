e0e322b588dc1a3d2609f27f96519b68e231a342
kaseredu <kaseredu@gmail.com>
2017-06-03 11:57:28 +0800

    initial commit
    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var person = {
+	name: "Hey",
+	age: 13
+}
+
+var serviceslist = {
+
+}
+
+var services = document.getElementById("servicescontainer");
+
+var categorieslist = {
+
+}
+
+var categories = document.getElementById("categorieslist");
+
+/*for(var i = 0; i < 10; i++) {
+	var bookcontainer = document.createElement("div");
+	bookcontainer.innerHTML = "book";
+	categories.appendChild(bookcontainer);
+}*/
+
+categories.innerText = "book";
+
